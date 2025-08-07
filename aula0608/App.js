0ef8ec4e233e5a77377d67b42e7fc4c3b3cb7c4d
GUILHERME CHASSOT SCHORR <guilherme.schorr@acad.univates>
2025-08-06 21:59:09 -0300

    Estudos independentes 2 com Flatlist

--- aula0608/App.js
+++ aula0608/App.js
@@ -1,5 +1,25 @@
 import React from "react";
-import { View, Text, Alert, Button, Switch } from "react-native";
+import { View, Text, Alert, Button, Switch, FlatList } from "react-native";
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
 
 export default function App() {
   return (
@@ -19,6 +39,11 @@ export default function App() {
       />
 
       <Switch value={true} />
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
